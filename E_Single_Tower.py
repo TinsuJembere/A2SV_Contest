ec85fb07f2a04148d3f582ef5bf36b84dc1162ee
Tinsu Jembere <tinsaejembere4@gmail.com>
2026-02-21 12:00:30 +0300

    more problem fixed

--- E_Single_Tower.py
+++ E_Single_Tower.py
@@ -1,37 +1,27 @@
-import sys
-
-
-input_data = sys.stdin.read().split()
-n = int(input_data[0])
-idx = 1
+n = int(input().strip())
 towers = []
 all_blocks = []
 
 for _ in range(n):
-    k = int(input_data[idx])
-    idx += 1
-    current_tower = []
-    for _ in range(k):
-        val = int(input_data[idx])
-        current_tower.append(val)
-        all_blocks.append(val)
-        idx += 1
-    towers.append(current_tower)
-
-sorted_blocks = sorted(all_blocks)
+    curr_tower = []
+    data = list(map(int, input().split()))
+    k = data[0]
+    block = data[1:]
+    towers.append(block)
+    all_blocks.extend(block)
+sorted_block = sorted(all_blocks)
 successor = {}
-for i in range(len(sorted_blocks) - 1):
-    successor[sorted_blocks[i]] = sorted_blocks[i+1]
-    
-splits = 0
+for i in range(len(sorted_block) - 1):
+    successor[sorted_block[i]] = sorted_block[i + 1]
+
+split = 0
 for tower in towers:
     for i in range(len(tower) - 1):
         u = tower[i]
-        v = tower[i+1]
-        if u not in successor or successor[u] != v:
-            splits += 1
-            
-total_segments = n + splits
-combines = total_segments - 1
+        v = tower[i + 1]
 
-print(f"{splits} {combines}")
+        if u not in successor or successor[u] != v:
+            split += 1
+total_segments = n + split
+combine = total_segments - 1
+print(split, combine)
